6c09059c9edcd35a1ba2e7b4baf4502a7bb9b0cb
rowdyslav <mrdoxy960@gmail.com>
2023-05-10 18:39:58 +0300

    Algo

--- cogs/Farmingpool.py
+++ cogs/Farmingpool.py
@@ -3,6 +3,12 @@ from discord.ext import commands, tasks
 # import discord
 
 
+def json_find(verst, key, value):
+    for dict_index in range(len(verst)):
+        if verst[dict_index][key] == value:
+            return dict_index
+
+
 class Farmingpool(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -24,15 +30,16 @@ class Farmingpool(commands.Cog):
                           for channel in guild.voice_channels
                           if channel.name.startswith('Farming APR')]
         first_r = requests.get(self.apis[0])
+        first_r = requests.get(self.apis[0])
         if first_r.status_code == 200:
-            first = first_r.json()[93]['apr24h']
+            first = first_r.json()[json_find(first_r.json(), "ammId", "GvRj43J4Mk93rmS8VFPVHEHDafrUwmbH625RgpPafZjQ")]['apr24h']
             print(first)
         else:
             print('Произошла ошибка при запросе к API')
             return
         second_r = requests.get(self.apis[1])
         if second_r.status_code == 200:
-            second = float(second_r.json()['data'][363]['apr'][:-1])
+            second = float(second_r.json()['data'][json_find(second_r.json()["data"], "id", "HP7Pg9MJ6HhRWJRPryxCZ1ziXr7H8qFWBQrcuLQUrLp3")]['apr'][:-1])
             print(second)
         else:
             print('Произошла ошибка при запросе к API')
